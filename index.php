03c5c39c5b72ae9470ce11269d8202afb7a8f2ab
eruhachi <gwyneth_schmale@hotmail.nl>
2020-01-08 14:19:00 +0100

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CMS</title>
+    <link rel="stylesheet" href="css/UniStyle.css">
+  </head>
+  <body>
+<p>tentoe</p>
+  </body>
+</html>
