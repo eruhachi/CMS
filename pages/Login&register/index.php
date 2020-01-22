edec8e8e0512cc51a650edec6193e906de31428e
zangrada <sandan01@live.nl>
2020-01-22 12:49:28 +0100

    Login form geupdate

--- pages/Login&register/index.php
+++ pages/Login&register/index.php
@@ -1,16 +1,8 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div class="header">
-		<h2>Home Page</h2>
-	</div>
-	<div class="content">
-	</div>
-		
+<H1> Je bent nu ingelogd </h1>
 </body>
 </html>
