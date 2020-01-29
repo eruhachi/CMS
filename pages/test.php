d4b324f0926c2de2d54cdf265eb23d4246cfc15f
RowanNorder <rmnorder@gmail.com>
2020-01-29 10:20:50 +0100

    textbox aangepast

--- pages/test.php
+++ pages/test.php
@@ -29,7 +29,7 @@
 		<br>
 		<label>a</label>
 		<br>
-		<input type="text">
+		<input type="text" id="invullen">
 	</form>
 
 	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores et deleniti eius labore aspernatur veniam nobis velit nihil, laudantium, tempore voluptas? Incidunt temporibus cum aspernatur in non maiores cupiditate voluptas?
@@ -67,7 +67,12 @@
 	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus obcaecati rerum libero debitis nemo quo eligendi numquam, soluta ullam dolorum sequi id aperiam illo iure a amet officiis iusto nihil!
 	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet accusamus modi dolorem, aut amet eius culpa quam deserunt, voluptate odio ex inventore optio voluptas voluptatum in nam asperiores ipsam hic!
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla recusandae totam consequatur obcaecati earum officia eaque quibusdam sapiente inventore! Mollitia labore aliquid eligendi debitis eius tempore blanditiis temporibus quod voluptates.
- 
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ullam animi similique quae autem, eaque quis ipsa repellendus aut, veniam, dolores corrupti. Aspernatur deleniti quis, aliquam ducimus non molestias temporibus.
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio recusandae eius culpa ipsam sint atque officia, officiis sapiente similique accusantium assumenda voluptatem illum laborum minus adipisci explicabo aliquam nemo eveniet.
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum provident deserunt omnis eius aut vel accusantium quaerat quisquam aliquam ullam veritatis, commodi veniam, eveniet alias, corporis inventore? Voluptatem, harum tempora!
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea blanditiis voluptatum illo voluptates dolorum? Repellat, maiores accusamus, suscipit optio deleniti hic quia facere vel obcaecati, iusto consequuntur magnam error eaque.
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae quae facere, alias quod harum voluptatum repellendus fuga deleniti dolor aspernatur est mollitia, laudantium aperiam praesentium? Consequatur nam vero accusantium minus!
+
 	</div>
 	</div>
 
