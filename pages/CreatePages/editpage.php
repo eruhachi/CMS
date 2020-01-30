28c181375d3f1d906db40cd1376993188b871b8b
damianvlegels <djwvlegels@gmail.com>
2020-01-30 11:33:17 +0100

    Update editpage.php

--- pages/CreatePages/editpage.php
+++ pages/CreatePages/editpage.php
@@ -16,3 +16,16 @@ if ($conn->query($sql) === TRUE) {
 $conn->close();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
