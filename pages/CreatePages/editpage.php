d3e0a5f7675f1c2ae5e13098879a0e26524e4745
damianvlegels <djwvlegels@gmail.com>
2020-01-30 10:47:54 +0100

    Update editpage.php

--- pages/CreatePages/editpage.php
+++ pages/CreatePages/editpage.php
@@ -2,47 +2,17 @@
 
 
 require "dbh.php";
-require "../Login&register/includes/dbh.inc.php";
+//require "../Login&register/includes/dbh.inc.php";
 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('', '', '')";
+$sql = "UPDATE cms_pages SET page_content='test', page_title='test' WHERE page_ID=2";
 
 if ($conn->query($sql) === TRUE) {
-    $last_id = $conn->insert_id;
-    echo "New record created successfully. Last inserted ID is: " . $last_id;
+    echo "Record updated successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error updating record: " . $conn->error;
 }
 
 $conn->close();
-
-
-$result = mysql_query("SELECT * FROM replies WHERE rid = '$id'");
-$test = mysql_fetch_array($result);
-if (!$result) 
-        {
-        die("Error: Data not found..");
-        }
-                $trigger1=$test['trigger1'] ;
-                $reply2= $test['reply2'] ;              
-
-if (isset($_POST['save']))
-{   
-    $triggera = $_POST['trigger1'];
-    $replyb = $_POST['reply2'];
-
-    mysql_query("UPDATE `replies`(trigger,reply) VALUES ('$triggera','$replyb') WHERE rid = '$id'");  
-    echo "Saved!";
-
-}
-mysql_close($conn);
 
 ?>
